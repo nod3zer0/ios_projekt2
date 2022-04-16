ea5e258f28fea7a9600f2bafeaa75fbe738c2385
xceska06 <xceska06@stud.fit.vutbr.cz>
2022-04-16 18:25:22 +0200

    finished refactor

---
+++ test.py
@@ -0,0 +1,303 @@
+#!/usr/bin/python3
+import subprocess
+from colorama import Fore
+from os.path import exists
+import os
+import sys
+
+allPassed = True
+
+testCnt = 0
+failedCnt = 0
+testFailed = False
+testRunning = False
+showOut = False
+
+def err(text):
+
+	global allPassed
+	global testFailed
+	testFailed = True
+	allPassed = False
+	print("[" + Fore.RED + "ERR" + Fore.WHITE + "] " + text)
+
+def note(text):
+	print("[ " + Fore.CYAN + "*" + Fore.WHITE + " ] " + text)
+
+def succ(text):
+	print("[" + Fore.GREEN + "OK" + Fore.WHITE + " ] " + text);
+
+def test(text):
+	global testFailed
+	global testCnt
+	global testRunning
+	testEnd()
+	testFailed = False
+	testRunning = True
+	testCnt+=1
+	print("[ " + Fore.BLUE + "T" + Fore.WHITE + " ] " + "Test for: " + text)
+	preclean()
+
+def testEnd():
+	global testFailed
+	global testRunning
+	global failedCnt
+	if testRunning:
+		if testFailed:
+			err("Test failed")
+			failedCnt += 1
+		else:
+			succ("Test passed")
+		if showOut and testFailed:
+			note("Printing ./proj2.out to terminal:")
+			if exists("./proj2.out"):
+				with open('./proj2.out', 'r') as f:
+					print(f.read())
+			else:
+				err("File ./proj2.out doesn't exists")
+	testRunning = False
+	testFailed = False
+
+for x in sys.argv[1:]:
+	if x=="--show-out":
+		showOut = True
+	else:
+		err(f"Unknown argument {x}")
+
+
+note("Test script has started")
+
+if not exists("./proj2"):
+	err("Script has to be in same directory as ./proj2")
+	exit(1)
+
+def preclean():
+	os.system("pkill proj2")
+	os.system("rm proj2.out 2>/dev/null")
+
+def postclean():
+	if "proj2" in subprocess.check_output(["ps"]).decode("utf-8"):
+		err("Proj2 is still running after process exited (unterminated childs)")
+		os.system("pkill proj2")
+		note("Proj2 is killed automatically now")
+
+def processFail(params):
+	proc = subprocess.Popen(["./proj2"] + params, stdin=subprocess.DEVNULL, stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
+	try:
+		outs, errs = proc.communicate(timeout=5)
+		if outs != "":
+			err("There shouldnt be any text on stdout")
+		if errs.strip() == "":
+			err("Missing error output on stderr")
+	except subprocess.TimeoutExpired:
+		proc.kill()
+		err("Process timed out")
+		postclean()
+		return
+	proc.wait()
+	postclean()
+	if proc.returncode != 1:
+		err("Wrong return code, should be set to 1")
+
+def processSucess(NO, NH, TI, TB):
+	proc = subprocess.Popen(["./proj2", str(NO), str(NH), str(TI), str(TB)], stdin=subprocess.DEVNULL, stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
+	try:
+		outs, errs = proc.communicate(timeout=5)
+		if outs != "":
+			err("There shouldnt be any text on stdout")
+		if errs != "":
+			err("There shouldnt be any errors on stderr")
+	except subprocess.TimeoutExpired:
+		proc.kill()
+		err("Process timed out")
+		postclean()
+		return
+	if not exists("proj2.out"):
+		err("Missing file proj2.out")
+	else:
+		dataFile = open("proj2.out")
+		lineCnt = 0
+		moleculeCnt = 0
+		oarr = [0]*NO
+		harr = [0]*NH
+		for line in dataFile.readlines():
+			line = line.strip()
+			fields = line.split(": ")
+			if len(fields) != 3:
+				err("Wrong format of line (expected \"xxx: xxx: xxx\") got:")
+				note(line)
+				break
+			lineCnt += 1
+			if fields[0] != str(lineCnt):
+				err(f"Wrong id of line, expected {lineCnt}")
+				note("Line: " + line)
+				break
+			if len(fields[1].split(" ")) != 2:
+				err(f"Expected atom type and id (ex. \"H 1\") got \"{fields[1]}\"")
+				note("Line: " + line)
+				break
+			type, id = fields[1].split(" ")
+			arr=None
+			if type == "O":
+				arr = oarr
+			elif type == "H":
+				arr = harr
+			else:
+				err(f"Unknown atom type \"{type}\" (expected \"O\" or \"H\")")
+				note("Line: " + line)
+				continue
+			if int(id)>len(arr):
+				err(f"Too big id of atom (max is {len(arr)}, found {int(id)})")
+				note("Line: " + line)
+				continue
+			cmd = fields[2].split(" ")[0]
+			id = int(id)-1
+			if cmd == "started":
+				if fields[2] != "started":
+					err("Text should be \"started\", found \"{fields[2]}\"")
+					note("Line: " + line)
+				if arr[id] != 0:
+					err("Starting atom which was already started")
+					note("Line: " + line)
+					continue
+				arr[id] = 1
+			elif cmd == "going":
+				if fields[2] != "going to queue":
+					err(f"text should be \"going to queue\", found \"{fields[2]}\"")
+					note("Line: " + line)
+				if arr[id] < 1:
+					err("Trying to put atom into queue, which wasn't started")
+					note("Line: " + line)
+					continue
+				if arr[id]>1:
+					err("Trying to put atom into queue, which was already in queue")
+					note("Line: " + line)
+					continue
+				arr[id] = 2
+			elif cmd == "creating":
+				if arr[id] < 2:
+					err("Trying to create molecule with atom, which wasn't in queue")
+					note("Line: " + line)
+					continue
+				if arr[id] > 2:
+					err("Trying to create molecule with atom, which was already used to create molecle")
+					note("Line: " + line)
+				arr[id] = 3
+			elif cmd == "molecule":
+				if arr[id] < 3:
+					err("Trying to finnish creation of molecule which hasn't yet started")
+					note("Line: " + line)
+					continue
+				if arr[id] > 3:
+					err("Trying to finnish creation of molecule which was already created")
+					note("Line: " + line)
+					continue
+				arr[id] = 4
+			elif cmd == "not":
+				if type == "O":
+					if fields[2] != "not enough H":
+						err("Text should be \"not enough H\", found \"{fields[2]}\"")
+						note("Line: " + line)
+				else:
+					if fields[2] != "not enough O or H":
+						err("Text should be \"not enough O or H\", found \"{fields[2]}\"")
+						note("Line: " + line)
+				if arr[id] < 2:
+					err("Atom needs to be in queue before figuring out it can't make molecule")
+					note("Line: " + line)
+					continue
+				if arr[id] > 2:
+					err("Atom failed making molecule after it started")
+					note("Line: " + line)
+					continue
+				arr[id] = 4
+			else:
+				err("Unknown action of atom")
+				note("Line: " + line)
+		faults = ["wasn't started", "didn't went to queue", "didn't attempted to create molecule", "didn't finnished forming of molecule"]
+		for i, x in enumerate(oarr):
+			if x<4:
+				err(f"Oxygen {i+1} {faults[x]}")
+		for i, x in enumerate(harr):
+			if x<4:
+				err(f"Hydrogen {i+1} {faults[x]}")
+		dataFile.close()
+	proc.wait()
+	postclean()
+	if proc.returncode != 0:
+		err("Wrong return code, should be set to 0")
+
+test("Makefile")
+if not exists("./Makefile"):
+	err("Makefile doesn't exists")
+else:
+	note("Automatically running make")
+	if os.system("make"):
+		err("Error while executing make")
+
+note("Following tests expect program to fail")
+
+test("No arguments")
+processFail([])
+test("Two arguments")
+processFail(["1", "2"])
+test("Three arguments")
+processFail(["1", "2", "3"])
+test("Five arguments")
+processFail(["1", "2", "3", "4", "5"])
+
+
+test("Negative NO")
+processFail(["-1", "1", "1", "1"])
+test("NO not a number")
+processFail(["1a", "1", "1", "1"])
+test("Missing NO")
+processFail(["", "1", "1", "1"])
+
+test("Negative NH")
+processFail(["1", "-1", "1", "1"])
+test("NH not a number")
+processFail(["1", "1a", "1", "1"])
+test("Missing NH")
+processFail(["1", "", "1", "1"])
+
+test("Negative TI")
+processFail(["1", "1", "-1", "1"])
+test("Too big TI")
+processFail(["1", "1", "1001", "1"])
+test("TI not a number")
+processFail(["1", "1", "1a", "1"])
+test("Missing TI")
+processFail(["1", "1", "", "1"])
+
+test("Negative TB")
+processFail(["1", "1", "1", "-1"])
+test("Too big TB")
+processFail(["1", "1", "1", "1001"])
+test("TB not a number")
+processFail(["1", "1", "1", "1a"])
+test("Missing TB")
+processFail(["1", "1", "1", ""])
+
+
+note("Following tests expect program to succeed")
+
+test("No atoms (0, 0, 100, 100)")
+processSucess(0, 0, 100, 100)
+
+test("Not enough of atoms for molecule (1, 1, 100, 100)")
+processSucess(1, 1, 100, 100)
+
+test("Creating one molecule (1, 2, 100, 100)")
+processSucess(1, 2, 100, 100)
+
+testEnd()
+note("Test script has finnished")
+note(f"Total of {testCnt} tests were run, {failedCnt} failed")
+
+if allPassed:
+	print(Fore.GREEN + "All tests have passed!!! :)" + Fore.WHITE)
+else:
+	if not showOut:
+		note("You can try running the script as ./test.py --show-out to show ./proj2.out on failed tests")
+	print(Fore.RED + "Some tests have failed :(" + Fore.WHITE)
--- testingfoleder/proj2.c
+++ testingfoleder/proj2.c
@@ -1,3 +1,4 @@
+// Online C compiler to run C program online
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/types.h>
@@ -25,14 +26,6 @@ sem_t *SharedHydrogenCount_sem;
 sem_t *SharedOxygenCount_sem;
 sem_t *WriteOut_sem;
 
-/**
- * @brief Converts string to int
- *
- * @param str
- * @param number
- * @return true on succes
- * @return false on failer
- */
 bool convertToInt(char str[], int *number)
 {
     char *endptr;
@@ -44,22 +37,13 @@ bool convertToInt(char str[], int *number)
     return true;
 }
 
-/**
- * @brief Safely increases shared memory
- *
- * @param SharedWriteoutCount
- */
-void incSharedMemory(int *SharedWriteoutCount)
+void incSharedMemory(int *shared_memory)
 {
     sem_wait(SharedMemory_sem);
-    (*SharedWriteoutCount)++;
+    (*shared_memory)++;
     sem_post(SharedMemory_sem);
 }
-/**
- * @brief Safely decreases Hydrogen count
- *
- * @param SharedWriteoutCount
- */
+
 void decSharedHydrogen(int *SharedHydrogenCount)
 {
     sem_wait(SharedHydrogenCount_sem);
@@ -67,11 +51,7 @@ void decSharedHydrogen(int *SharedHydrogenCount)
     (*SharedHydrogenCount)--;
     sem_post(SharedHydrogenCount_sem);
 }
-/**
- * @brief Safely decreases Oxygen count
- *
- * @param SharedWriteoutCount
- */
+
 void decSharedOxygen(int *SharedOxygenCount)
 {
     sem_wait(SharedOxygenCount_sem);
@@ -79,106 +59,65 @@ void decSharedOxygen(int *SharedOxygenCount)
     (*SharedOxygenCount)--;
     sem_post(SharedOxygenCount_sem);
 }
-/**
- * @brief Safely increases Molecule count
- *
- * @param SharedWriteoutCount
- */
+
 void incMoleculeCount(int *SharedMoleculeCount)
 {
     sem_wait(SharedMoleculeCount_sem);
     (*SharedMoleculeCount)++;
     sem_post(SharedMoleculeCount_sem);
 }
-/**
- * @brief Oxygen process
- *
- * @param id oxygen id
- * @param TI maximum time for which it will wait until it goes to queue
- * @param TB maximum time for which it will be creating molecule
- * @param SharedWriteoutCount shared varriable which stores number of writes to output
- * @param SharedMoleculeCount shared varriable which stores number of molecules
- * @param SharedOxygenCount shared varriable which stores number of oxygens
- * @param SharedHydrogenCount shared varriable which stores number of hydrogens
- * @param fp pointer to output file
- */
-void Oxygen(int id, int TI, int TB, int *SharedWriteoutCount, int *SharedMoleculeCount, int *SharedOxygenCount, int *SharedHydrogenCount, FILE *fp)
+
+void kyslik(int id, int TI, int TB, int *shared_memory, int *SharedMoleculeCount, int *SharedOxygenCount, int *SharedHydrogenCount, FILE *fp)
 {
-
-    // safely increses SharedWriteoutCount and writes to output file
     sem_wait(WriteOut_sem);
-    incSharedMemory(SharedWriteoutCount);
-    fprintf(fp, "%d: O %d: started\n", *SharedWriteoutCount, id);
+    incSharedMemory(shared_memory);
+    fprintf(fp, "%d: O %d: started\n", *shared_memory, id);
     sem_post(WriteOut_sem);
-
-    // waits for random time in intervall <0,TI>
     usleep(rand() % TI);
-
-    // safely increses SharedWriteoutCount and writes to output file
     sem_wait(WriteOut_sem);
-    incSharedMemory(SharedWriteoutCount);
-    fprintf(fp, "%d: O %d: going to queue\n", *SharedWriteoutCount, id);
+    incSharedMemory(shared_memory);
+    fprintf(fp, "%d: O %d: going to queue\n", *shared_memory, id);
     sem_post(WriteOut_sem);
-
-    // enters queue (waits at semaphore)
     sem_wait(O_sem);
-
     incMoleculeCount(SharedMoleculeCount);
-
-    // takes two hydrogens from queue
     sem_post(H_sem);
     sem_post(H_sem);
-
-    // if there is not enough hydrogens, it writes message and ends
     if (*SharedHydrogenCount < 2)
     {
         sem_wait(WriteOut_sem);
-        incSharedMemory(SharedWriteoutCount);
-        fprintf(fp, "%d: O %d: not enough H\n", *SharedWriteoutCount, id);
+        incSharedMemory(shared_memory);
+        fprintf(fp, "%d: O %d: not enough H\n", *shared_memory, id);
         sem_post(WriteOut_sem);
         decSharedOxygen(SharedOxygenCount);
 
         exit(0);
     }
-    // waits until 2 hydrogens start creating molecule
     sem_wait(O_sem2);
     sem_wait(O_sem2);
 
-    // if there is not enough hydrogens, it writes message and ends
     if (*SharedHydrogenCount < 2)
     {
         sem_wait(O_sem);
-        incSharedMemory(SharedWriteoutCount);
-        fprintf(fp, "%d: O %d: not enough H\n", *SharedWriteoutCount, id);
+        incSharedMemory(shared_memory);
+        fprintf(fp, "%d: O %d: not enough H\n", *shared_memory, id);
         sem_post(WriteOut_sem);
         decSharedOxygen(SharedOxygenCount);
 
         exit(0);
     }
-
-    incSharedMemory(SharedWriteoutCount);
-
-    // starts creating molecule
+    incSharedMemory(shared_memory);
     sem_wait(WriteOut_sem);
-    fprintf(fp, "%d: O %d: creating molecule %d\n", *SharedWriteoutCount, id, *SharedMoleculeCount);
+    fprintf(fp, "%d: O %d: creating molecule %d\n", *shared_memory, id, *SharedMoleculeCount);
     sem_post(WriteOut_sem);
     usleep(rand() % TB);
-    // when finished lets hydrogens know that molecule is finished
     sem_post(H_sem2);
     sem_post(H_sem2);
-
-    // writes that molecule is finished
     sem_wait(WriteOut_sem);
-    incSharedMemory(SharedWriteoutCount);
-    fprintf(fp, "%d: O %d: molecule %d created\n", *SharedWriteoutCount, id, *SharedMoleculeCount);
+    incSharedMemory(shared_memory);
+    fprintf(fp, "%d: O %d: molecule %d created\n", *shared_memory, id, *SharedMoleculeCount);
     sem_post(WriteOut_sem);
-
-    // lets next oxygen out from queu
     sem_post(O_sem);
-
     decSharedOxygen(SharedOxygenCount);
-
-    // if there are no more oxygens it lets all hydrogens free from queue
     if (*SharedOxygenCount == 0)
     {
         for (int i = 0; i <= *SharedHydrogenCount; i++)
@@ -190,34 +129,17 @@ void Oxygen(int id, int TI, int TB, int *SharedWriteoutCount, int *SharedMolecul
     exit(0);
 }
 
-/**
- * @brief Oxygen process
- *
- * @param id oxygen id
- * @param TI maximum time for which it will wait until it goes to queue
- * @param SharedWriteoutCount shared varriable which stores number of writes to output
- * @param SharedMoleculeCount shared varriable which stores number of molecules
- * @param SharedOxygenCount shared varriable which stores number of oxygens
- * @param SharedHydrogenCount shared varriable which stores number of hydrogens
- * @param fp pointer to output file
- */
-void Hydrogen(int id, int TI, int *shared_memory, int *SharedMoleculeCount, int *SharedOxygenCount, int *SharedHydrogenCount, FILE *fp)
+void vodik(int id, int TI, int TB, int *shared_memory, int *SharedMoleculeCount, int *SharedOxygenCount, int *SharedHydrogenCount, FILE *fp)
 {
-    // safely increses SharedWriteoutCount and writes to output file
     sem_wait(WriteOut_sem);
     incSharedMemory(shared_memory);
     fprintf(fp, "%d: H %d: started\n", *shared_memory, id);
     sem_post(WriteOut_sem);
-    // waits for random time in intervall <0,TI>
     usleep(rand() % TI);
-
-    // safely increses SharedWriteoutCount and writes to output file
     sem_wait(WriteOut_sem);
     incSharedMemory(shared_memory);
     fprintf(fp, "%d: H %d: going to queue\n", *shared_memory, id);
     sem_post(WriteOut_sem);
-
-    // if there is not enough hydrogens or oxygens, it writes message and ends
     if (*SharedHydrogenCount < 2 || *SharedOxygenCount < 1)
     {
         sem_wait(WriteOut_sem);
@@ -228,11 +150,7 @@ void Hydrogen(int id, int TI, int *shared_memory, int *SharedMoleculeCount, int 
 
         exit(0);
     }
-
-    // waits until oxygen lets it to create molecule
     sem_wait(H_sem);
-
-    // if there is not enough hydrogens or oxygens, it writes message and ends
     if (*SharedHydrogenCount < 2 || *SharedOxygenCount < 1)
     {
         sem_wait(WriteOut_sem);
@@ -243,16 +161,13 @@ void Hydrogen(int id, int TI, int *shared_memory, int *SharedMoleculeCount, int 
 
         exit(0);
     }
-    // safely writes that it is creting molecule
+
     sem_wait(WriteOut_sem);
     incSharedMemory(shared_memory);
     fprintf(fp, "%d: H %d: creating molecule %d\n", *shared_memory, id, *SharedMoleculeCount);
     sem_post(WriteOut_sem);
-    // lets oxygen know that it is ready to create molecule
     sem_post(O_sem2);
-    // waits until hydrogen says that molecule is created
     sem_wait(H_sem2);
-    // if there is not enough hydrogens or oxygens, it writes message and ends
     if (*SharedHydrogenCount < 2 || *SharedOxygenCount < 1)
     {
         sem_wait(WriteOut_sem);
@@ -263,15 +178,12 @@ void Hydrogen(int id, int TI, int *shared_memory, int *SharedMoleculeCount, int 
 
         exit(0);
     }
-
-    // writes that molecule is finished
     sem_wait(WriteOut_sem);
+    usleep(rand() % TB);
     incSharedMemory(shared_memory);
     fprintf(fp, "%d: H %d: molecule %d created\n", *shared_memory, id, *SharedMoleculeCount);
     sem_post(WriteOut_sem);
-
     decSharedHydrogen(SharedHydrogenCount);
-    // if there is not enought hydrogens, it lets all hydrogens and oxygens free from queue
     if (*SharedHydrogenCount < 2)
     {
         for (int i = 0; i <= *SharedHydrogenCount; i++)
@@ -289,11 +201,6 @@ void Hydrogen(int id, int TI, int *shared_memory, int *SharedMoleculeCount, int 
     exit(0);
 }
 
-/**
- * @brief main process
- *
- * @return int
- */
 int main(int argc, char **argv)
 {
     FILE *fp;
@@ -301,49 +208,46 @@ int main(int argc, char **argv)
     int TI = -1;
     int NO = -1;
     int NH = -1;
-
-    // checking if arguments are correct
     if (argc != 5)
     {
         fprintf(stderr, "Wrong argument count\n");
-        return 1;
+         return 1;
     }
-    if (!convertToInt(argv[1], &NO))
+
+    if (!convertToInt(argv[1], &NO) )
     {
-        fprintf(stderr, "Invalid argument NO\n");
-        return 1;
+         fprintf(stderr, "Invalid argument NO\n");
+          return 1;
     }
-    if (!convertToInt(argv[2], &NH))
+    if (!convertToInt(argv[2], &NH) )
     {
-        fprintf(stderr, "Invalid argument NH\n");
-        return 1;
+         fprintf(stderr, "Invalid argument NH\n");
+          return 1;
     }
-    if (!convertToInt(argv[3], &TI))
+    if (!convertToInt(argv[3], &TI) )
     {
-        fprintf(stderr, "Invalid argument TI\n");
-        return 1;
+         fprintf(stderr, "Invalid argument TI\n");
+          return 1;
     }
     if (!convertToInt(argv[4], &TB))
     {
-        fprintf(stderr, "Invalid argument TB\n");
-        return 1;
+         fprintf(stderr, "Invalid argument TB\n");
+              return 1;
     }
-    if (TB < 0 || TI < 0 || NH < 0 || NO < 0)
+    if (TB < 0|| TI < 0|| NH < 0|| NO < 0)
     {
-        fprintf(stderr, "No negative numbers aloved in any argument\n");
-        return 1;
+         fprintf(stderr, "No negative numbers aloved in any argument\n");
+              return 1;
     }
-    if (TB > 1000 || TI > 1000)
+      if (TB > 1000|| TI > 1000)
     {
-        fprintf(stderr, "TB or TI have to be less or equal to 1000");
-        return 1;
+         fprintf(stderr, "TB or TI have to be less or equal to 1000");
+              return 1;
     }
 
-    //converts miliseconds to microseconds
-    TI = 1000*TI;
-    TB = 1000*TB;
 
-    // tries to create output file
+
+
     fp = fopen("proj2.out", "w");
     setbuf(fp, NULL);
     if (fp == NULL)
@@ -352,7 +256,6 @@ int main(int argc, char **argv)
         return 1;
     }
 
-    // unlinks all semaphores who may be there from previous runs
     sem_unlink("O_sem_filee");
     sem_unlink("O_sem2_filee");
     sem_unlink("H_sem_filee");
@@ -363,7 +266,6 @@ int main(int argc, char **argv)
     sem_unlink("SharedHydrogenCount_sem");
     sem_unlink("WriteOut_sem");
 
-    // opens all needed sempahores
     if ((WriteOut_sem = sem_open("WriteOut_sem", O_CREAT, 0777, 1)) == SEM_FAILED)
     {
         printf("%d", errno);
@@ -377,12 +279,12 @@ int main(int argc, char **argv)
     if ((SharedOxygenCount_sem = sem_open("SharedOxygenCount_sem", O_CREAT, 0777, 1)) == SEM_FAILED)
     {
         printf("%d", errno);
-        return 1;
+         return 1;
     }
     if ((SharedHydrogenCount_sem = sem_open("SharedHydrogenCount_sem", O_CREAT, 0777, 1)) == SEM_FAILED)
     {
         printf("%d", errno);
-        return 1;
+         return 1;
     }
     if ((O_sem = sem_open("O_sem_filee", O_CREAT, 0777, 1)) == SEM_FAILED)
     {
@@ -398,12 +300,12 @@ int main(int argc, char **argv)
     if ((H_sem = sem_open("H_sem_filee", O_CREAT, 0660, 0)) == SEM_FAILED)
     {
         printf("%d", errno);
-        return 1;
+         return 1;
     }
     if ((H_sem2 = sem_open("H_sem2_filee", O_CREAT, 0660, 0)) == SEM_FAILED)
     {
         printf("%d", errno);
-        return 1;
+         return 1;
     }
     if ((SharedMemory_sem = sem_open("SharedMemory_sem_filee", O_CREAT, 0777, 1)) == SEM_FAILED)
     {
@@ -411,7 +313,6 @@ int main(int argc, char **argv)
         return 1;
     }
 
-    // creates all shared varriables
     int *shared_memory = mmap(NULL, 4, PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, -1, 0);
     int *SharedMoleculeCount = mmap(NULL, 4, PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, -1, 0);
     int *SharedHydrogenCount = mmap(NULL, 4, PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, -1, 0);
@@ -420,34 +321,28 @@ int main(int argc, char **argv)
     *SharedMoleculeCount = 0;
     *SharedOxygenCount = atoi(argv[1]);
     *SharedHydrogenCount = atoi(argv[2]);
-
-    // inicializes random number generator with time as seed
     time_t t;
     srand((unsigned)time(&t));
 
-    // creates oxygen child proceses
-    for (int i = 0; i < NO; i++)
+    for (int i = 0; i < atoi(argv[1]); i++) // loop will run n times (n=5)
     {
         if (fork() == 0)
         {
-            Oxygen(i + 1, TI, TB, shared_memory, SharedMoleculeCount, SharedOxygenCount, SharedHydrogenCount, fp);
-        }
-    }
-    // creates hydrogen child proceses
-    for (int i = 0; i < NH; i++)
-    {
-        if (fork() == 0)
-        {
-            Hydrogen(i + 1, TI, shared_memory, SharedMoleculeCount, SharedOxygenCount, SharedHydrogenCount, fp);
+            kyslik(i + 1, atoi(argv[3]), atoi(argv[4]), shared_memory, SharedMoleculeCount, SharedOxygenCount, SharedHydrogenCount, fp);
         }
     }
 
-    int status = 0;
-    pid_t pid;
-    // parrent waits for all his children to die
-    while ((pid = wait(&status)) > 0)
-        ;
-    // cleans and closes all files,semaphores and shared varriables
+    for (int i = 0; i < atoi(argv[2]); i++) // loop will run n times (n=5)
+    {
+        if (fork() == 0)
+        {
+            vodik(i + 1, atoi(argv[3]), atoi(argv[4]), shared_memory, SharedMoleculeCount, SharedOxygenCount, SharedHydrogenCount, fp);
+        }
+    }
+
+    for (int i = 0; i < atoi(argv[1]) + atoi(argv[2]); i++) // loop will run n times (n=5)
+        wait(NULL);
+
     sem_close(O_sem);
     sem_close(O_sem2);
     sem_close(H_sem);
@@ -466,10 +361,6 @@ int main(int argc, char **argv)
     sem_unlink("O_sem2_filee");
     sem_unlink("SharedOxygenCount_sem");
     sem_unlink("SharedHydrogenCount_sem");
-    munmap(shared_memory, 4);
-    munmap(SharedMoleculeCount, 4);
-    munmap(SharedHydrogenCount, 4);
-    munmap(SharedOxygenCount, 4);
     fclose(fp);
     return 0;
 }
